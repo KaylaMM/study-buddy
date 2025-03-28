1653b1d8b50628ab471f75b64d3ef78971a4a3ec
Kayla Martinez <kaylamartinez1201@gmail.com>
2025-03-28 16:04:24 -0400

    Remove comments, create decks and flashcards table

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,8 +1,7 @@
--- Create the database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS studybuddy;
 USE studybuddy;
 
--- Create users table
+
 CREATE TABLE IF NOT EXISTS users (
     id INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(50) UNIQUE NOT NULL,
