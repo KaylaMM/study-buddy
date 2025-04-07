f192bbbfbc61329abb59fd453047e0579cbd6c02
Kayla Martinez <kaylamartinez1201@gmail.com>
2025-04-07 12:15:46 -0400

    Update import on DashboardPage to resolve deployment error

--- client/src/pages/DashboardPage/DashboardPage.jsx
+++ client/src/pages/DashboardPage/DashboardPage.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate, Outlet, useLocation } from "react-router-dom";
-import NavBar from "../../components/NavBar/Navbar";
+import NavBar from "../../components/NavBar/NavBar";
 import Deck from "../../components/Deck/Deck";
 import AiFlashcardForm from "../../components/AiFlashcardForm/AiFlashcardForm";
 import { deckService } from "../../services/deckService";
